766c66190064f3c7ed74300d8a75ac8c092a3592
Ubuntu <ubuntu@ip-172-31-93-137.ec2.internal>
2020-12-02 20:39:47 +0000

    updated Dockerfile

--- OpenViEWS2/runners/import_data.py
+++ OpenViEWS2/runners/import_data.py
@@ -2,7 +2,7 @@
 
 import argparse
 import logging
-from typing import Optional, Tuple
+# from typing import Optional, Tuple
 import views
 
 logging.basicConfig(
@@ -16,32 +16,32 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
-def parse_args() -> Tuple[Optional[str], bool, bool]:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--path_zip", type=str, help="Path to zip to import from",
-    )
-    parser.add_argument(
-        "--fetch", action="store_true", help="Fetch from website."
-    )
-    parser.add_argument(
-        "--datasets", action="store_true", help="Refresh datasets."
-    )
-    args = parser.parse_args()
+# def parse_args() -> Tuple[Optional[str], bool, bool]:
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         "--path_zip", type=str, help="Path to zip to import from",
+#     )
+#     parser.add_argument(
+#         "--fetch", action="store_true", help="Fetch from website."
+#     )
+#     parser.add_argument(
+#         "--datasets", action="store_true", help="Refresh datasets."
+#     )
+#     args = parser.parse_args()
+# 
+#     if args.path_zip and args.fetch:
+#         raise RuntimeError("Pass in --path_zip or --fetch, not both.")
+# 
+#     return args.path_zip, args.fetch, args.datasets
 
-    if args.path_zip and args.fetch:
-        raise RuntimeError("Pass in --path_zip or --fetch, not both.")
 
-    return args.path_zip, args.fetch, args.datasets
-
-
-def run_import_tables_and_geoms(path_zip) -> None:
+def run_import_tables_and_geoms(path_zip):
     views.apps.data.public.import_tables_and_geoms(
         tables=views.TABLES, geometries=views.GEOMETRIES, path_zip=path_zip,
     )
 
 
-def refresh_datasets() -> None:
+def refresh_datasets():
 
     log.info("Started refreshing all datasets.")
 
@@ -52,20 +52,18 @@ def refresh_datasets() -> None:
         "pgm_africa_imp_0",
     ]
     for dataset_name in datasets_to_update:
-        log.info(f"Started refreshing dataset {dataset_name}")
         views.DATASETS[dataset_name].refresh()
 
     log.info("Finished refreshing all imp_0 datasets.")
 
 
-def main() -> None:
+def main():
 
-    path_zip, do_fetch, do_datasets = parse_args()
-
-    if do_fetch:
-        path_zip = views.apps.data.public.fetch_latest_zip_from_website(
+    # path_zip, do_fetch, do_datasets = parse_args()
+    
+    path_zip = views.apps.data.public.fetch_latest_zip_from_website(
             path_dir_destination=views.DIR_SCRATCH
-        )
+    )
 
     run_import_tables_and_geoms(path_zip)
     refresh_datasets()
@@ -73,3 +71,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
